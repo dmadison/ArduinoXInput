98ea8e92943b4fd09fab76a41cb8bc0b65726559
David Madison <dmadison@users.noreply.github.com>
2019-03-30 08:58:34 -0400

    Add library-less USB API demo
    For testing boards packages without having to use the library. Included in the 'extras' folder so as not to confuse end-users.

---
+++ extras/API-Demo/API-Demo.ino
@@ -0,0 +1,82 @@
+/*
+ *  Project     Arduino XInput Library
+ *  @author     David Madison
+ *  @link       github.com/dmadison/ArduinoXInput
+ *  @license    MIT - Copyright (c) 2019 David Madison
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *  Example:      API Demo
+ *  Description:  Tests for the functionality of the XInputUSB API
+ *                without using the Arduino XInput library. Useful
+ *                for testing boards packages without the library
+ *                as a factor.
+ *
+ *                Tests the following XInputUSB functions:
+ *                  * static boolean connected(void)
+ *                  * static uint8_t available(void)
+ *                  * static int send(const void *buffer, uint8_t nbytes)
+ *                  * static int recv(void *buffer, uint8_t nbytes)
+ *                  * static void setRecvCallback(void(*callback)(void))
+ *
+ *                If the example is running properly, the controller's
+ *                'A' button should toggle on and off every 2 seconds.
+ */
+
+#ifndef USB_XINPUT
+#error "USB_XINPUT not defined! No XInput API detected"
+#else
+
+uint8_t txData[20] = { 0x00, 0x14, 0x00 };
+uint8_t rxData[8]  = { 0x00 };
+
+boolean buttonState = false;
+
+void setup() {
+	while (!XInputUSB::connected()) {}  // wait for connection
+	XInputUSB::setRecvCallback(receiveCallback);
+}
+
+void loop() {
+	buttonState = !buttonState;
+	setButtonA(txData, buttonState);
+	XInputUSB::send(txData, sizeof(txData));
+	
+	delay(1000);
+}
+
+void setButtonA(uint8_t * ptr, boolean state) {
+	const uint8_t ButtonIndex = 3;
+	const uint8_t ButtonPosition = 4;
+
+	if (state == true) {
+		ptr[ButtonIndex] |= (1 << ButtonPosition);
+	}
+	else {
+		ptr[ButtonIndex] &= ~(1 << ButtonPosition);
+	}
+}
+
+void receiveCallback() {
+	if (XInputUSB::available() > 0) {
+		XInputUSB::recv(rxData, sizeof(rxData));
+	}
+}
+
+#endif
