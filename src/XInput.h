31c8071832a87d2d57b762a1c33e5c64da943743
Nick Gutierrez <60188339+ng0805@users.noreply.github.com>
2022-11-01 17:39:08 -0400

    Add second joystick

--- src/XInput.h
+++ src/XInput.h
@@ -1,170 +1,101 @@
-/*
- *  Project     Arduino XInput Library
- *  @author     David Madison
- *  @link       github.com/dmadison/ArduinoXInput
- *  @license    MIT - Copyright (c) 2019 David Madison
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
+#include <XInput.h>
 
-#ifndef XInput_h
-#define XInput_h
+const int StartButtonPin = 0;
+const int SelectButtonPin = 2;
 
-#include <Arduino.h>
+const int AButtonPin = 3;
+const int XButtonPin = 4;
+const int YButtonPin = 5;
+const int BButtonPin = 6;
 
-enum XInputControl : uint8_t {
-	BUTTON_LOGO = 0,
-	BUTTON_A = 1,
-	BUTTON_B = 2,
-	BUTTON_X = 3,
-	BUTTON_Y = 4,
-	BUTTON_LB = 5,
-	BUTTON_RB = 6,
-	BUTTON_BACK = 7,
-	BUTTON_START = 8,
-	BUTTON_L3 = 9,
-	BUTTON_R3 = 10,
-	DPAD_UP = 11,
-	DPAD_DOWN = 12,
-	DPAD_LEFT = 13,
-	DPAD_RIGHT = 14,
-	TRIGGER_LEFT = 15,
-	TRIGGER_RIGHT = 16,
-	JOY_LEFT,
-	JOY_RIGHT,
-};
+const int RBButtonPin = 7;
+const int LBButtonPin = 8;
 
-enum class XInputReceiveType : uint8_t {
-	Rumble = 0x00,
-	LEDs = 0x01,
-};
+const int LeftJoyXPin = A3;
+const int LeftJoyYPin = A4;
+//const int LeftJoyPushPin = A5;
 
-enum class XInputLEDPattern : uint8_t {
-	Off = 0x00,
-	Blinking = 0x01,
-	Flash1 = 0x02,
-	Flash2 = 0x03,
-	Flash3 = 0x04,
-	Flash4 = 0x05,
-	On1 = 0x06,
-	On2 = 0x07,
-	On3 = 0x08,
-	On4 = 0x09,
-	Rotating = 0x0A,
-	BlinkOnce = 0x0B,
-	BlinkSlow = 0x0C,
-	Alternating = 0x0D,
-};
+const int RightJoyXPin = A0;
+const int RightJoyYPin = A1;
+const int led1 = 9;
+const int led2 = 10;
+const int led3 = 11;
+const int led4 = 12;
+const int led5 = 13;
+const int led6 = 1;
+const int led7 = 17;
+const int led8 = 16;
 
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  
+  pinMode(StartButtonPin, INPUT_PULLUP);
+  pinMode(SelectButtonPin, INPUT_PULLUP);
 
-class XInputController {
-public:
-	XInputController();
+  pinMode(AButtonPin, INPUT_PULLUP);
+  pinMode(XButtonPin, INPUT_PULLUP);
+  pinMode(YButtonPin, INPUT_PULLUP);
+  pinMode(BButtonPin, INPUT_PULLUP);
 
-	void begin();
+  pinMode(RBButtonPin, INPUT_PULLUP);
+  pinMode(LBButtonPin, INPUT_PULLUP);
 
-	// Set Control Surfaces
-	void press(uint8_t button);
-	void release(uint8_t button);
-	void setButton(uint8_t button, boolean state);
+  XInput.setRange(JOY_LEFT, 0, 1023);  // using analogRead (10-bit ADC)
+  XInput.setRange(JOY_RIGHT, 0, 1023);  // using analogRead (10-bit ADC)
+  //pinMode(LeftJoyPushPin, INPUT_PULLUP);
+	XInput.begin();
+}
 
-	void setDpad(XInputControl pad, boolean state);
-	void setDpad(boolean up, boolean down, boolean left, boolean right, boolean useSOCD=true);
+void loop() {
+  boolean StartState = digitalRead(StartButtonPin);
+	XInput.setButton(BUTTON_START, StartState);   // press Start conditionally
+  digitalWrite(led1, StartState);
 
-	void setTrigger(XInputControl trigger, int32_t val);
+  boolean SelectState = digitalRead(SelectButtonPin);
+	XInput.setButton(BUTTON_BACK, SelectState);   // press Select conditionally
+  digitalWrite(led2, SelectState);
 
-	void setJoystick(XInputControl joy, int32_t x, int32_t y);
-	void setJoystick(XInputControl joy, boolean up, boolean down, boolean left, boolean right, boolean useSOCD=true);
-	void setJoystickX(XInputControl joy, int32_t x, boolean invert=false);
-	void setJoystickY(XInputControl joy, int32_t y, boolean invert=false);
+	boolean AState = digitalRead(AButtonPin);
+	XInput.setButton(BUTTON_A, AState);   // press A conditionally
+  digitalWrite(led3, AState);
 
-	void releaseAll();
+  boolean XState = digitalRead(XButtonPin);
+	XInput.setButton(BUTTON_X, XState);   // press X conditionally
+  digitalWrite(led4, XState);
 
-	// Auto-Send Data
-	void setAutoSend(boolean a);
+  boolean YState = digitalRead(YButtonPin);
+	XInput.setButton(BUTTON_Y, YState);   // press Y conditionally
+  digitalWrite(led5, YState);
 
-	// Read Control Surfaces
-	boolean getButton(uint8_t button) const;
-	boolean getDpad(XInputControl dpad) const;
-	uint8_t getTrigger(XInputControl trigger) const;
-	int16_t getJoystickX(XInputControl joy) const;
-	int16_t getJoystickY(XInputControl joy) const;
+  boolean BState = digitalRead(BButtonPin);
+	XInput.setButton(BUTTON_B, BState);   // press B conditionally
+  digitalWrite(led6, BState);
 
-	// Received Data
-	uint8_t getPlayer() const;  // Player # assigned to the controller (0 is unassigned)
+  boolean RBState = digitalRead(RBButtonPin);
+	XInput.setButton(BUTTON_RB, RBState);   // press RB conditionally
+  digitalWrite(led7, RBState);
 
-	uint16_t getRumble() const;  // Rumble motors. MSB is large weight, LSB is small
-	uint8_t  getRumbleLeft() const;  // Large rumble motor, left grip
-	uint8_t  getRumbleRight() const; // Small rumble motor, right grip
+  boolean LBState = digitalRead(LBButtonPin);
+	XInput.setButton(BUTTON_LB, LBState);   // press LB conditionally
+  digitalWrite(led8, LBState);
 
-	XInputLEDPattern getLEDPattern() const;  // Returns LED pattern type
+  int LeftJoyXState = analogRead(LeftJoyXPin);
+  int LeftJoyYState = analogRead(LeftJoyYPin);
+  XInput.setJoystickX(JOY_LEFT, LeftJoyXState, false);
+  XInput.setJoystickY(JOY_LEFT, LeftJoyYState, true); //Y axis needed to be inverted for correct positioning
 
-	// Received Data Callback
-	using RecvCallbackType = void(*)(uint8_t packetType);
-	void setReceiveCallback(RecvCallbackType);
+  // boolean LeftJoyPushState = !digitalRead(LeftJoyPushPin);
+  // XInput.setButton(BUTTON_L3, LeftJoyPushState);   // press L3 Right conditionally
 
-	// USB IO
-	boolean connected();
-	int send();
-	int receive();
-
-	// Control Input Ranges
-	struct Range { int32_t min; int32_t max; };
-
-	void setTriggerRange(int32_t rangeMin, int32_t rangeMax);
-	void setJoystickRange(int32_t rangeMin, int32_t rangeMax);
-	void setRange(XInputControl ctrl, int32_t rangeMin, int32_t rangeMax);
-
-	// Setup
-	void reset();
-
-	// Debug
-	void printDebug(Print& output=Serial) const;
-
-private:
-	// Sent Data
-	uint8_t tx[20];  // USB transmit data
-	boolean newData;  // Flag for tx data changed
-	boolean autoSendOption;  // Flag for automatically sending data
-	
-	void setJoystickDirect(XInputControl joy, int16_t x, int16_t y);
-
-	void inline autosend() {
-		if (autoSendOption) { send(); }
-	}
-
-	// Received Data
-	volatile uint8_t player;  // Gamepad player #, buffered
-	volatile uint8_t rumble[2];  // Rumble motor data in, buffered
-	volatile XInputLEDPattern ledPattern;  // LED pattern data in, buffered
-	RecvCallbackType recvCallback;  // User-set callback for received data
-
-	void parseLED(uint8_t leds);  // Parse LED data and set pattern/player data
-
-	// Control Input Ranges
-	Range rangeTrigLeft, rangeTrigRight, rangeJoyLeft, rangeJoyRight;
-	Range * getRangeFromEnum(XInputControl ctrl);
-	static int32_t rescaleInput(int32_t val, const Range& in, const Range &out);
-	static int16_t invertInput(int16_t val, const Range& range);
-};
-
-extern XInputController XInput;
-
-#endif
+  int RightJoyXState = analogRead(RightJoyXPin);
+  int RightJoyYState = analogRead(RightJoyYPin);
+  XInput.setJoystickX(JOY_RIGHT, RightJoyXState, false);
+  XInput.setJoystickY(JOY_RIGHT, RightJoyYState, true); //Y axis needed to be inverted for correct positioning
+}
